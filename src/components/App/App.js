265411408351d9f7d8d641bac6ae9da8122f1493
Magdalena Pawłowska <mag.pawloowska@gmail.com>
2018-01-13 17:40:08 +0100

    Import sass file

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,8 +3,8 @@ import { Route, Switch } from "react-router-dom";
 import "normalize.css";
 import Header from '../Header/Header';
 import Listing from '../Listing/Listing';
-// import '../../sass/main.scss';
-require('../../styles/main.scss');
+import '../../styles/main.scss';
+
 
 const App = (props) => {
     return (
